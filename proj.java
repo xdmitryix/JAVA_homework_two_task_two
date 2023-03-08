5946273554da7a9f4f5bf77ec984d8a5cbc4dd85
dmitry <rbdeman@yandex.ru>
2023-03-08 18:40:09 +0300

    программа работает

--- proj.java
+++ proj.java
@@ -2,9 +2,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-
-import javax.swing.text.html.StyleSheet;
 
 // Дана строка (получение через обычный текстовый файл!!!)
 
@@ -25,13 +22,14 @@ public class proj {
     public static void main(String[] args) {
         String student = "Студент";
         String estimation = "получил";
-        String subject = "предмет";
-        readFile();
+        String subject = "по предмету";
+        String probel = " ";
+        readFile(student,estimation,subject,probel);
         
     }
 
 
-    public static void readFile(){
+    public static void readFile(String stud, String estim, String sobj, String prob){
         String buffer;
         File file = new File("dataList.txt");
         try (BufferedReader br = new BufferedReader(new FileReader(file)))
@@ -39,33 +37,23 @@ public class proj {
             String line;
             while ((line = br.readLine()) != null) {
                 buffer = line;
-                System.out.println(buffer);
                 String[] parStr = parser(buffer);
-                System.out.println(Arrays.toString(parStr));
                 String secName = parStr[0];
-                System.out.println(secName);
                 String ocenka = parStr[1];
-                System.out.println(ocenka); 
                 String sub = parStr[2];
-                System.out.println(sub);
                 String[] parseSecName = parserTwo(secName);
-                System.out.println(Arrays.toString(parseSecName));
                 String[] parseOcenka = parserTwo(ocenka);
-                System.out.println(Arrays.toString(parseOcenka));
                 String[] parseSub = parserTwo(sub);
-                System.out.println(Arrays.toString(parseSub));
                 String onlySecName = parseSecName[1];
-                System.out.println(onlySecName);
                 String onlyOcenka = parseOcenka[1];
-                System.out.println(onlyOcenka); 
                 String onlySub = parseSub[1];
-                System.out.println(onlySub);
                 String finaleSecname = onlySecName.substring(1, onlySecName.length()-1);
-                System.out.println(finaleSecname);
                 String finaleOcenka = onlyOcenka.substring(1, onlyOcenka.length()-1);
-                System.out.println(finaleOcenka);
                 String finaleSub = onlySub.substring(1, onlySub.length()-1);
-                System.out.println(finaleSub);
+                StringBuilder user_char = new StringBuilder();
+                user_char.append(stud).append(prob).append(finaleSecname).append(prob).append(estim).append(prob).append(finaleOcenka).append(prob).append(sobj).append(prob) 
+                .append(finaleSub);
+                System.out.println(user_char);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -75,7 +63,6 @@ public class proj {
 
     public static String[] parser(String oneLine){
         String[] words = oneLine.split(",");
-        System.out.println(Arrays.toString(words)); 
         return words;
     }
 
@@ -83,7 +70,4 @@ public class proj {
         String[] wordsTwo = oneLine.split(":"); 
         return wordsTwo;
     }
-
-    
-    
 }
